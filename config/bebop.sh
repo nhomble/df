67d960995586a6edafc9a8055fd2088da6a37014
nhomble <nhomble@terpmail.umd.edu>
2022-04-18 16:57:51 -0400

    path updates for npm/pip/gin

--- config/bebop.sh
+++ config/bebop.sh
@@ -5,3 +5,10 @@ alias python='python3'
 
 export PATH="$HOME/.basher/bin:$PATH"   ##basher5ea843
 eval "$(basher init - zsh)"             ##basher5ea843
+
+export PATH=$PATH:$(npm bin)
+export PATH=$PATH:$(npm bin -g)
+
+export PATH="$HOME/bin:$PATH"
+export PATH="/Users/nicolas/Library/Application Support/gin/bin:$PATH"
+export PATH="/Users/nicolas/Library/Python/3.8/bin:$PATH"
