c9fe5adb2a081c7aa1dc6485a99068637d38ab59
nhomble <nhomble@terpmail.umd.edu>
2022-04-27 23:05:53 -0700

    chruby

--- config/bebop.sh
+++ config/bebop.sh
@@ -13,3 +13,6 @@ export PATH=$PATH:$(npm bin -g)
 export PATH="$HOME/bin:$PATH"
 export PATH="/Users/nicolas/Library/Application Support/gin/bin:$PATH"
 export PATH="/Users/nicolas/Library/Python/3.8/bin:$PATH"
+
+source $HOMEBREW_PREFIX/opt/chruby/share/chruby/chruby.sh
+chruby ruby-3.1.2
