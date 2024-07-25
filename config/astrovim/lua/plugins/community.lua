1ede2f3b41241556925ddb3cbfb2384a24f69a6c
nhomble <nhomble@terpmail.umd.edu>
2024-07-25 15:26:22 -0400

    use community

--- config/astrovim/lua/plugins/community.lua
+++ config/astrovim/lua/plugins/community.lua
@@ -2,4 +2,10 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.colorscheme.tokyodark-nvim" },
   { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.pack.rego" },
+  { import = "astrocommunity.pack.terraform" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.test.neotest" },
 }
