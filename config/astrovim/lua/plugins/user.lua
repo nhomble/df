cb2fc806d639cb111bb72596d192d05fb077e198
nhomble <nhomble@terpmail.umd.edu>
2024-08-22 11:08:23 -0400

    moon plugin

--- config/astrovim/lua/plugins/user.lua
+++ config/astrovim/lua/plugins/user.lua
@@ -4,6 +4,23 @@
 ---@type LazySpec
 return {
 	{
+		"nhomble/moon-monorepo.nvim",
+		dependencies = {
+			"nvim-neo-tree/neo-tree.nvim",
+			opts = function(_, opts)
+				local moon = require("neo-tree.sources.moon-monorepo")
+				table.insert(opts.sources, moon.name)
+
+				if moon.is_supported() then
+					table.insert(opts.source_selector.sources, {
+						display_name = moon.display_name,
+						source = moon.name,
+					})
+				end
+			end,
+		},
+	},
+	{
 		"nhomble/thought-flow.nvim",
 		opts = {
 			notifications = {
