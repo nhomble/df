7d550ef6c7481e55893dd3496e19c7093ed624b3
nhomble <nhomble@terpmail.umd.edu>
2024-09-15 01:49:31 -0400

    update

--- config/astrovim/lua/plugins/mason.lua
+++ config/astrovim/lua/plugins/mason.lua
@@ -10,7 +10,7 @@ return {
       ensure_installed = {
         "lua_ls",
         "jsonls",
-        "tsserver",
+        "ts_ls",
         "typos_lsp"
         -- add more arguments for adding more language servers
       },
