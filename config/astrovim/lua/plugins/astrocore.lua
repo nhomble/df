6295f91dde6b642a2997d67aee8aa26dcec4d343
nhomble <nhomble@terpmail.umd.edu>
2024-09-15 00:57:01 -0400

    formatting / descriptions / add more thought-flow commands

--- config/astrovim/lua/plugins/astrocore.lua
+++ config/astrovim/lua/plugins/astrocore.lua
@@ -11,11 +11,11 @@ return {
 		-- Configure core features of AstroNvim
 		features = {
 			large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
-			autopairs = true, -- enable autopairs at start
-			cmp = true, -- enable completion at start
-			diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
-			highlighturl = true, -- highlight URLs at start
-			notifications = true, -- enable notifications at start
+			autopairs = true,                              -- enable autopairs at start
+			cmp = true,                                    -- enable completion at start
+			diagnostics_mode = 3,                          -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
+			highlighturl = true,                           -- highlight URLs at start
+			notifications = true,                          -- enable notifications at start
 		},
 		-- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
 		diagnostics = {
@@ -24,14 +24,14 @@ return {
 		},
 		-- vim options can be configured here
 		options = {
-			opt = { -- vim.opt.<key>
+			opt = {              -- vim.opt.<key>
 				relativenumber = true, -- sets vim.opt.relativenumber
-				number = true, -- sets vim.opt.number
-				spell = false, -- sets vim.opt.spell
+				number = true,     -- sets vim.opt.number
+				spell = false,     -- sets vim.opt.spell
 				signcolumn = "yes", -- sets vim.opt.signcolumn to yes
-				wrap = false, -- sets vim.opt.wrap
+				wrap = false,      -- sets vim.opt.wrap
 			},
-			g = { -- vim.g.<key>
+			g = {                -- vim.g.<key>
 				-- configure global vim variables (vim.g)
 				-- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
 				-- This can be found in the `lua/lazy_setup.lua` file
@@ -72,7 +72,7 @@ return {
 				-- https://github.com/jbyuki/one-small-step-for-vimkind/blob/main/doc/osv.txt#L60
 				["<Leader>dNs"] = {
 					function()
-						require("osv").launch({port=8086})
+						require("osv").launch({ port = 8086 })
 					end,
 					desc = "Start nvim-lua debug server"
 				},
@@ -86,6 +86,7 @@ return {
 					function()
 						require("osv").run_this()
 					end,
+					desc = "Run current file in nvim-lua debug server"
 				},
 				-- thought flow
 				["<Leader>j"] = {
